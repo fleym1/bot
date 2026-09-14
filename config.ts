c24a9c056e53b88e93765451bbf50339e90314f1
fleym1 <nikitakent008@gmail.com>
2026-09-14 11:09:19 +0600

    обнова

--- config.ts
+++ config.ts
@@ -7,3 +7,17 @@ string => {
         throw new Error('${key} is not defined in environment variables');
     return value;
 };
+
+const API_KEY = "КЛЮЧ_СЮДА";
+const TASKS_URL = "https://yougile.com/api-v2/task-list";
+
+const MY_COLUMN_ID = "айди колонки сюда";
+
+const PRIORITY_STICKER_ID = "de4408e1-ff26-4e43-81bb-d11dd87651db";
+const IMPORTANT_STATES = [
+  "dcf5fcc816b0",
+  "eb85a8d85664",
+];
+
+const REFRESH_INTERVAL = 30000;
+const DESCRIPTION_MAX_LENGTH = 150;
